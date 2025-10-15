45c37d82b3b7ee9f332f0a4e96c8d7632c2d5b6c
ItsTheSmit <max_smith@mines.edu>
2025-10-14 20:30:36 -0600

    main

---
+++ Python/Demo 1/main.py
@@ -0,0 +1,21 @@
+
+# Demo 1 Main
+#
+# Max Smith & Garret Reader
+# 10/13/25
+#
+# Description
+
+# Imports for CV
+import cv2 as cv
+from cv2 import aruco
+import numpy as np
+import time
+
+def main():
+    capture = cv.VideoCapture(0)
+
+# End main
+
+if __name__ == "__main__":
+    main() # Run main program
