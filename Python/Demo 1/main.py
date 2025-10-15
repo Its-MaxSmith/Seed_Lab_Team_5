247c41d41ef3865401e5e15f82fac94b56217723
ItsTheSmit <max_smith@mines.edu>
2025-10-14 21:28:54 -0600

    main

--- Python/Demo 1/main.py
+++ Python/Demo 1/main.py
@@ -12,10 +12,37 @@ from cv2 import aruco
 import numpy as np
 import time
 
+# Imports for function files
+import LCD_print
+
+# Main program
 def main():
     capture = cv.VideoCapture(0)
 
+    i = 0 # For print delay
+    msg = 'No message' # For degree message
+
+    while True:
+        isTrue, frame = capture.read()
+
+        #----------------
+        # Degree Finding Function
+        #----------------
+
+        if (i >= 50):
+            LCD_print.LCD_print(msg)
+            i = 0
+
+        i += 1
+
+        if cv.waitKey(20) & 0xFF == ord('d'):
+            # Clear pointers and windows
+            capture.release()
+            cv.destroyAllWindows()
+            break
+
 # End main
 
+# Run main
 if __name__ == "__main__":
     main() # Run main program
